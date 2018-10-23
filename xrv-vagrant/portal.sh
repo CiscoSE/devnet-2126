6cd74fc3085983d250f305c00636c64876dcec91
jasmah <jasmah@gmail.com>
2018-10-23 11:24:10 -0600

    removed cisco from clone url

--- xrv-vagrant/portal.sh
+++ xrv-vagrant/portal.sh
@@ -5,7 +5,7 @@ sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 
 
-git clone https://www.github.cisco.com/CiscoSE/devnet-2126.git
+git clone https://www.github.com/CiscoSE/devnet-2126.git
 cd devnet-2126/nso_clive-portal/
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
